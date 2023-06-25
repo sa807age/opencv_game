730b0533046dc1720d93512a6f98827f1f10e3e2
nir <nir123211@gmail.com>
2023-06-25 19:06:47 +0300

    removed unnecessary files

--- new_game/main2.py
+++
@@ -1,8 +0,0 @@
-import cv2 as cv
-
-from new_game.round_class import Round
-
-round1_img_path = '../media/photos/pexels-david-bartus-1166209.jpg'
-round1_img = cv.imread(round1_img_path)
-round1 = Round(round1_img, 5, 5, 5)
-round1.start_round()
