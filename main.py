4c53c1b27eca3df9866be82886649e1921f141c6
nir123211 <nir123211@gmail.com>
2024-06-27 20:30:18 +0300

    bug fix

--- main.py
+++ main.py
@@ -12,21 +12,21 @@ from scripts.tutorial import play_tutorial
 round_img_path = 'media/photos/first_round.jpg'
 round_img = cv.imread(round_img_path)
 Round(image=round_img, sniper_max_ammo=5, launcher_ammo=1, sniper_zoom=2, time=45, round_music=game_music1,
-      y_location=2500, spawn_chance=1 / 200, headers=['Round 1', "Let's go"]).play_round()
+      horizon_line=2600, spawn_chance=1 / 200, headers=['Round 1', "Let's go"]).play_round()
 
 
 round_img_path = 'media/photos/second_round.jpg'
 round_img = cv.imread(round_img_path)
 Round(image=round_img, sniper_max_ammo=5, launcher_ammo=1, sniper_zoom=5, time=45, round_music=game_music3,
-      y_location=1500, spawn_chance=1 / 200, headers=['Round 2', 'Wrong scope :(']).play_round()
+      horizon_line=1500, spawn_chance=1 / 200, headers=['Round 2', 'Wrong scope :(']).play_round()
 
 round_img_path = 'media/photos/third_round.jpg'
 round_img = cv.imread(round_img_path)
 Round(image=round_img, sniper_max_ammo=1, launcher_ammo=2, sniper_zoom=3, time=45, round_music=game_music4,
-      y_location=2500, spawn_chance=1 / 200, headers=['Round 3', 'No magazine!']).play_round()
+      horizon_line=2500, spawn_chance=1 / 200, headers=['Round 3', 'No magazine!']).play_round()
 
 
 round_img_path = 'media/photos/forth_round.jpg'
 round_img = cv.imread(round_img_path)
 Round(image=round_img, sniper_max_ammo=1000, launcher_ammo=3, sniper_zoom=1, time=120, round_music=game_music2,
-      y_location=1800, spawn_chance=1 / 50, headers=['Last Round', 'Seiko Mode']).play_round()
+      horizon_line=1800, spawn_chance=1 / 50, headers=['Last Round', 'Seiko Mode']).play_round()
