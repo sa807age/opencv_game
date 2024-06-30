8231b5d1be96841b7765059032d6b069cde7bffa
nir123211 <nir123211@gmail.com>
2024-06-30 18:07:17 +0300

    utils.py- add docstring with GPT

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,22 +1,50 @@
-import math
-from random import random
 import cv2 as cv
 import numpy as np
+from random import random
+import math
 
 
 def rotate_vector(vector, angle):
-    # Convert angle to radians
-    angle = np.deg2rad(angle)
+    """
+    Rotate a 2D vector by a given angle in degrees.
 
-    # Create rotation matrix
-    rot_matrix = np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
+    Parameters
+    ----------
+    vector : array_like
+        A 2D vector to be rotated.
+    angle : float
+        The angle in degrees to rotate the vector.
 
     # Rotate vector
     rotated_vector = np.dot(rot_matrix, vector)
     return rotated_vector
+    Returns
+    -------
+    ndarray
+        The rotated vector.
+    """
 
 
 def draw_image_on_image(main_image, image_to_draw, location, size=1):
+    """
+    Draw an image on another image at a specified location and size.
+
+    Parameters
+    ----------
+    main_image : ndarray
+        The main image on which the other image will be drawn.
+    image_to_draw : ndarray
+        The image to be drawn on the main image.
+    location : tuple
+        The (x, y) coordinates where the image will be drawn.
+    size : float, optional
+        The scaling factor for resizing the image to draw (default is 1).
+
+    Returns
+    -------
+    bool or None
+        Returns True if the image was successfully drawn, None otherwise.
+    """
     if size != 1:
         image_to_draw = cv.resize(image_to_draw, (math.ceil(image_to_draw.shape[1] * size),
                                                   math.ceil(image_to_draw.shape[0] * size)))
@@ -56,6 +84,18 @@ def draw_image_on_image(main_image, image_to_draw, location, size=1):
 
 
 def put_round_text(frame, big_text, small_text):
+    """
+    Put round text at the center of the frame.
+
+    Parameters
+    ----------
+    frame : ndarray
+        The image frame on which the text will be put.
+    big_text : str
+        The main text to be displayed.
+    small_text : str
+        The secondary text to be displayed below the main text.
+    """
     header_position = (frame.shape[1] // 2, frame.shape[0] // 2)
     cv.putText(frame, big_text, (header_position[0] - 150, header_position[1] - 200),
                5, 3, (150, 150, 150), 3)
@@ -64,10 +104,38 @@ def put_round_text(frame, big_text, small_text):
 
 
 def probability(prob):
+    """
+    Returns True with a probability of 'prob'.
+
+    Parameters
+    ----------
+    prob : float
+        The probability threshold.
+
+    Returns
+    -------
+    bool
+        True if the random number is less than 'prob', False otherwise.
+    """
     return random() < prob
 
 
-def probability_two(prob1, prob2) -> [bool, bool]:
+def probability_two(prob1, prob2):
+    """
+    Returns two booleans with probabilities 'prob1' and 'prob2'.
+
+    Parameters
+    ----------
+    prob1 : float
+        The probability threshold for the first boolean.
+    prob2 : float
+        The probability threshold for the second boolean.
+
+    Returns
+    -------
+    tuple of bool
+        A tuple containing two booleans.
+    """
     result = random()
     return result < prob1, prob1 < result < prob1 + prob2
 
