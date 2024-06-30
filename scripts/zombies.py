9f5c7f838ab203baa052bbdfada857502718fe72
nir123211 <nir123211@gmail.com>
2024-06-30 18:16:38 +0300

    refactor with GPT

--- scripts/zombies.py
+++ scripts/zombies.py
@@ -8,9 +8,6 @@ import numpy as np
 from scripts.sounds import SoundManager
 from scripts.utils import rotate_vector, probability
 
-soldiers_death_sounds = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'sounds',
-                                                     'death_sounds'))
-
 
 class Zombie:
     zombies = []
@@ -103,8 +100,10 @@ class Zombie:
         list
             The bounding box coordinates and size.
         """
-        return [math.ceil(self.location[0] - 10 * self.size), math.ceil(self.location[1] - 20 * self.size),
-                math.ceil(20 * self.size), math.ceil(40 * self.size)]
+        return [math.ceil(self.location[0] - 10 * self.size),
+                math.ceil(self.location[1] - 20 * self.size),
+                math.ceil(20 * self.size),
+                math.ceil(40 * self.size)]
 
     def draw_arrow(self, frame, aim):
         """
@@ -120,16 +119,15 @@ class Zombie:
         arrow_color = (0, int(255 * (1600 - self.frames_lived) / 1600), int(255 * self.frames_lived / 1600))
         direction_vector = np.array([self.location[0] - aim.x, self.location[1] - aim.y])
         vector_size = np.linalg.norm(direction_vector)
-        normalized_vector = (direction_vector / vector_size)
-        arrow_point = [int((normalized_vector[0] * 550) + 600), int((normalized_vector[1] * 350) + 400)]
+        normalized_vector = direction_vector / vector_size
+        arrow_point = [int(normalized_vector[0] * 550 + 600), int(normalized_vector[1] * 350 + 400)]
         rotated_vector1 = rotate_vector(normalized_vector, 135) * (1 / math.sqrt(vector_size)) * 2000
         rotated_vector2 = rotate_vector(normalized_vector, -135) * (1 / math.sqrt(vector_size)) * 2000
+
         cv.line(frame, arrow_point, [int(arrow_point[0]) + int(rotated_vector1[0]),
-                                     int(arrow_point[1]) + int(rotated_vector1[1])],
-                arrow_color, 10)
+                                     int(arrow_point[1]) + int(rotated_vector1[1])], arrow_color, 10)
         cv.line(frame, arrow_point, [int(arrow_point[0]) + int(rotated_vector2[0]),
-                                     int(arrow_point[1]) + int(rotated_vector2[1])],
-                arrow_color, 10)
+                                     int(arrow_point[1]) + int(rotated_vector2[1])], arrow_color, 10)
 
     def update(self):
         """
@@ -144,7 +142,8 @@ class Zombie:
             self.location[0] += random.randrange(-1, 2)
         self.location[1] += 0.12
         self.frames_lived += 1
-        self.size = (1.0017 ** self.frames_lived)
+        self.size = 1.0017 ** self.frames_lived
+
         if self.frames_lived == 1400:
             SoundManager.screaming.play()
         if self.frames_lived > 2000:
@@ -181,7 +180,7 @@ class Zombie:
         relative_location : list
             The relative location of the zombie.
         """
-        # eyebrows
+        # Eyebrows
         cv.line(photo, [relative_location[0] - math.ceil(6 * self.size),
                         relative_location[1] - math.ceil(12.5 * self.size)],
                 [relative_location[0] - math.ceil(3.5 * self.size),
@@ -190,13 +189,13 @@ class Zombie:
                         relative_location[1] - math.ceil(12.5 * self.size)],
                 [relative_location[0] + math.ceil(3.5 * self.size),
                  relative_location[1] - math.ceil(11.5 * self.size)], [0, 0, 0], math.ceil(0.5 * self.size))
-        # mouth
+        # Mouth
         cv.circle(photo, [relative_location[0], relative_location[1] - math.ceil(5 * self.size)],
                   math.ceil(2.5 * self.size), [0, 0, 0], -1)
 
     def draw_body(self, photo, relative_location: list):
         """
-        Draws the zombie's body on the photo.
+        Draw the zombie's body on the photo.
 
         Parameters
         ----------
@@ -209,25 +208,25 @@ class Zombie:
                   math.ceil(10 * self.size), self.color, -1)
         cv.line(photo, [relative_location[0], relative_location[1]],
                 [relative_location[0], relative_location[1] + math.ceil(10 * self.size)], self.color,
-                math.ceil(1 + 1 * self.size))
+                math.ceil(1 + self.size))
         cv.line(photo, [relative_location[0], relative_location[1] + math.ceil(3 * self.size)],
                 [relative_location[0] + math.ceil(10 * self.size), relative_location[1]],
-                self.color, math.ceil(1 + 1 * self.size))
+                self.color, math.ceil(1 + self.size))
         cv.line(photo, [relative_location[0], relative_location[1] + math.ceil(3 * self.size)],
                 [relative_location[0] - math.ceil(10 * self.size), relative_location[1]], self.color,
-                math.ceil(1 + 1 * self.size))
+                math.ceil(1 + self.size))
         cv.line(photo, [relative_location[0], relative_location[1] + math.ceil(10 * self.size)],
                 [relative_location[0] + math.ceil(7 * self.size),
                  relative_location[1] + math.ceil(20 * self.size)],
-                self.color, math.ceil(1 + 1 * self.size))
+                self.color, math.ceil(1 + self.size))
         cv.line(photo, [relative_location[0], relative_location[1] + math.ceil(10 * self.size)],
                 [relative_location[0] - math.ceil(7 * self.size),
                  relative_location[1] + math.ceil(20 * self.size)],
-                self.color, math.ceil(1 + 1 * self.size))
+                self.color, math.ceil(1 + self.size))
 
     def draw_face(self, photo, relative_location: list):
         """
-        Draws the zombie's face on the photo.
+        Draw the zombie's face on the photo.
 
         Parameters
         ----------
@@ -236,7 +235,7 @@ class Zombie:
         relative_location : list
             The relative location of the zombie.
         """
-        # eyes
+        # Eyes
         cv.circle(photo,
                   [relative_location[0] + math.ceil(5 * self.size), relative_location[1] - math.ceil(10 * self.size)],
                   math.ceil(0.2 * self.size), [0, 0, 0], -1)
